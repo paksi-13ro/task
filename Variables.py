79dfd5f6403ff2183a45b06aa0a835742ea41bc2
paksi-13ro <a9145795026@gmail.com>
2024-09-30 11:40:29 +0300

    new tesk

---
+++ Variables.py
@@ -0,0 +1,5 @@
+numberOfTasks = ("12")
+hours_spent = ("1.5")
+CourseName = ("Python")
+timeForOneTask = (float(hours_spent) / float(numberOfTasks))
+print("Курс:",CourseName,", всего задач:",numberOfTasks,", затраченно часов:",hours_spent,"часа", ",среднее время выполнения:",timeForOneTask,"часа.")
