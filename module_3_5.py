cd03efc9ccba33c6d817c9f84e254c08c16203c0
a9145 <a9145795026@gmail.com>
2024-11-18 12:36:27 +0300

    new task

--- module_3_5.py
+++ module_3_5.py
@@ -1,8 +1,6 @@
 def get_multiplied_digits(nomber):
     str_number = str(nomber)
     first = int(str_number[0])
-    while str_number.endswith('0'):
-        str_number = str_number[:len(str_number) - 1]
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
